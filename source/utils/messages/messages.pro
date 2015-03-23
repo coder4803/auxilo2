82758db28a9edf3e4f2a2cba6278d3d47e6de67b
coder4803 <coder4803@users.noreply.github.com>
2015-03-23 22:32:23 +0200

    Add classes to handle "set state" messages.

--- source/utils/messages/messages.pro
+++ source/utils/messages/messages.pro
@@ -4,11 +4,15 @@ QT          -= gui
 
 SOURCES     += message.cpp \
                signalmessage.cpp \
-               signalackmessage.cpp
+               signalackmessage.cpp \
+               setstatemessage.cpp \
+               setstateackmessage.cpp
 
 HEADERS     += message.h \
                signalmessage.h \
-               signalackmessage.h
+               signalackmessage.h \
+               setstatemessage.h \
+               setstateackmessage.h
 
 INCLUDEPATH += ../../../externals/include/
 
---
+++ source/utils/messages/setstateackmessage.h
@@ -0,0 +1,66 @@
+#include "message.h"
+
+#ifndef UTILS_SETSTATEACKMESSAGE_H
+#define UTILS_SETSTATEACKMESSAGE_H
+
+namespace Utils {
+
+/*!
+ * \brief The SetStateAckMessage class
+ * This class can be used to create and parse SetStateAck messages.
+ */
+class SetStateAckMessage : public Message
+{
+public:
+   /*!
+    * \brief Result of setting state.
+    */
+   enum Result {
+      SUCCEEDED,     //!< Setting state succeeded.
+      ILLEGAL_VALUE, //!< Tried to set illegal value.
+      FAILED         //!< Failed because of some other reason.
+   };
+
+   /*!
+    * \brief Constructor.
+    * This Constructor is used to create state set message from scratch.
+    * \param ackId Acknowledge id.
+    * \param result Result of setting state.
+    */
+   SetStateAckMessage(quint32 ackId,
+                      Result result);
+
+   /*!
+    * \brief Constructor.
+    * This constructor is used to create message from received binary data.
+    * \param data Received binary data.
+    */
+   SetStateAckMessage(QByteArray data);
+
+   /*!
+    * \brief Returns acknowledge id.
+    * \return Acknowledge id.
+    */
+   quint32 ackId() { return m_ackId; }
+
+   /*!
+    * \brief Returns message in binary format.
+    * \return Message in binary format.
+    */
+   QByteArray data() const;
+
+   /*!
+    * \brief Returns message in string format.
+    * This is useful when debugging.
+    * \return Message in string format.
+    */
+   QString string() const;
+
+private:
+   quint32 m_ackId;
+   Result m_result;
+};
+
+} // Utils
+
+#endif
