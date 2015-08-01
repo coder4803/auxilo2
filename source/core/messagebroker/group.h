c18e0f0321a22a3fc02cd57752813e833fefe497
coder4803 <coder4803@users.noreply.github.com>
2015-08-01 11:10:06 +0300

    Documentation for MessageBroker.

--- source/core/messagebroker/group.h
+++ source/core/messagebroker/group.h
@@ -8,20 +8,51 @@ namespace Core {
 
 class Connection;
 
+/*!
+ * \brief The Group class.
+ * This class describes one group. It takes care of sending messages to users
+ * which are joined to the group.
+ */
 class Group : public QObject
 {
    Q_OBJECT
 public:
+   /*!
+    * \brief Constructor.
+    * \param parent Parent object.
+    */
    explicit Group(QObject *parent = 0);
 
 public slots:
+   /*!
+    * \brief Adds new connection to this group.
+    * \param connection Pointer to the connection.
+    */
    void addConnection(Connection* connection);
+
+   /*!
+    * \brief Remove connection from the group.
+    * \param connection Pointer to the connection to remove.
+    */
    void removeConnection(Connection* connection);
+
+   /*!
+    * \brief Sends message to all group members.
+    * \param payload Message to send.
+    */
    void publishMessage(const QByteArray& payload);
+
+   /*!
+    * \brief Sends message to one member in the group.
+    * \param payload Message to send.
+    */
    void deliverMessage(const QByteArray& payload);
 
 private:
+   //! List of connections joined to this group.
    QSet<Connection*> m_connections;
+
+   //! Connection iterator. This is used when delivering messages.
    QSet<Connection*>::Iterator m_iter;
 };
 
--- source/core/messagebroker/messagebroker.h
+++ source/core/messagebroker/messagebroker.h
@@ -11,22 +11,63 @@
 
 namespace Core {
 
+/*!
+ * \brief The MessageBroker class.
+ * MessageBroker's task is to deliver messages between other core applications.
+ * It works a lot like simple IRC-server but has some extra features.
+ * This is main class of message broker. It takes care of handling connections
+ * and groups. It also handles data delivery on high level.
+ */
 class MessageBroker : public QObject
 {
    Q_OBJECT
 public:
+   /*!
+    * \brief Constructor.
+    * \param parent Parent object.
+    */
    explicit MessageBroker(QObject *parent = 0);
 
 private slots:
+   /*!
+    * \brief Handles new connections.
+    */
    void onNewConnection();
+
+   /*!
+    * \brief Inserts connection to a group.
+    * Note that "connection" is the sender of the signal!
+    * \param groupName Name of group the connection is inserted to.
+    */
    void onJoinToGroup(QString groupName);
+
+   /*!
+    * \brief Publishes message to a group.
+    * \param payload Message to publish.
+    * \param group Name of group the message is published to.
+    */
    void onPublishMessage(QByteArray payload, QString group);
+
+   /*!
+    * \brief Delivers message to group
+    * \param payload Message to delive.
+    * \param group Name of group the message is delivered to.
+    */
    void onDeliverMessage(QByteArray payload, QString group);
+
+   /*!
+    * \brief Handles client disconnections.
+    */
    void onDisconnect();
 
 private:
+   //! Server socket.
    QTcpServer* m_server;
+
+   //! List of connections.
    QList<Connection*> m_connections;
+
+   //! Hash of groups.
    QHash<QString, Group*> m_groups;
 };
 
