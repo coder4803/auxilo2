dbce73dddbc5db137da71ffbb7de3da9de4bad34
PerttuP <perttu.paarlahti@gmail.com>
2015-10-17 20:21:39 +0300

    Signal generator: added --help commandline argument.
    Signed-off-by: PerttuP <perttu.paarlahti@gmail.com>

--- source/core/signalgenerator/main.cpp
+++ source/core/signalgenerator/main.cpp
@@ -5,6 +5,20 @@
 
 int main(int argc, char *argv[])
 {
+    if (std::find_if(argv+1, argv+argc,
+                     [](char* str){return QString(str)=="--help";})
+            != argv+argc)
+    {
+        qCritical() << "Signal generator is part of the Auxilo2 core.";
+        qCritical() << "Signal generator is responsible for timing signals.";
+        qCritical() << "Optional commandline arguments:";
+        qCritical() << "   -v                     verbose mode.";
+        qCritical() << "   --cleardb              clear all previous events.";
+        qCritical() << "   --ui <ui name>         Use custom UI.";
+        qCritical() << "See developer wiki for more information.";
+        return 0;
+    }
+
     std::unique_ptr<QCoreApplication> a(nullptr);
     a.reset( SignalGenerator::UserInterface::initUI(argc, argv) );
     if (a == nullptr){
